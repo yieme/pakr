5345ebe5266308e1a55ffa2ecf1d0f82cd15d88f
yieme <yieme@cormea.com>
2015-05-21 14:45:04 -0600

    add favicon

--- index.js
+++ index.js
@@ -9,13 +9,31 @@ var proxyPackage  = require('proxy-cache-packages')
 var apiDoc        = require('./modules/api-doc')
 var apiLatest     = require('./modules/api-latest')
 var apiPackage    = require('./modules/api-package')
+var favicon       = require('serve-favicon')(__dirname + '/favicon.ico')
+var pakrStatic    = process.env.STATIC_URL || 'http://pakr-static.herokuapp.com'
+var domain        = {
+  pakr:       pakrStatic + '/$package/$version/$file', // proxy-cache-multi-domain override to include local files
+  cdnjs:      'https://cdnjs.cloudflare.com/ajax/libs/$package/$version/$file',
+  jsdelivr:   'https://cdn.jsdelivr.net/$package/$version/$file',
+  google:     'https://ajax.googleapis.com/ajax/libs/$package/$version/$file',
+  bootstrap:  'https://maxcdn.bootstrapcdn.com/$package/$version/$file',
+  bootswatch: 'https://maxcdn.bootstrapcdn.com/bootswatch/$version/$package/$file',
+}
+var packageList = [
+  'packages/strapdownjs/pakr.json',
+  'cdnall_data.json',
+]
+
 
 Dias(function(dias) {
   var logVariables  = { server: { id: pkg.name, ver: pkg.version, ua: dias.useragent, node: dias.node, pid: process.pid } }
   var logger        = new Logger(logVariables, false)
   var config        = {
     dir:        './tmp',
-    logRequest: true
+    logRequest: true,
+    domain:     domain,
+    skipInit:   true, // skip initial initialization in proxyPackage() below, as init will be done manually. avoids double loading
+    packageDataUrl: packageList
   }
   proxyPackage(config)
   var packages = proxyPackage.init()
@@ -41,8 +59,19 @@ Dias(function(dias) {
   }
 
   function logError(err, req, res, next) {
-    logger.error(err.stack)
-    res.status(500).send({ code: 500, error: err.message })
+    if ('string' == typeof err) {
+      if (err.indexOf('Not Found') >= 0) {
+        logger.info(err)
+        res.status(404).send({ code: 404, error: err.replace(': /', ': ') })
+        return
+      } else {
+        logger.warn(err)
+      }
+    } else {
+      logger.error(err)
+      if (err.stack) logger.error(err.stack)
+    }
+    res.status(500).send({ code: 500, error: err.message || err })
   }
 
   process.on('uncaughtException', function (err) {
@@ -54,7 +83,7 @@ Dias(function(dias) {
     logger: logger,
     pre:    [
       middleServer.log,
-      middleServer.ignoreFavicon,
+      favicon,
       apiDoc,
       apiLatest,
       apiPackage
